2bd2bcd894789e5849dd615d3c2f0847c2c26fa4
Lola Applegate <laurenemilya@gmail.com>
2022-03-31 21:34:48 -0700

    code for prompts, alerts, and arrays

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # password-generator
 UWBC HW#3 - JavaScript Password Generator
+
+## User Story
+
+As a user, I want to generate a random password. 
+When I click the button, I'm presented with a prompt and 4 confirm options to choose the length & character types of my password. 
+
+
+## My failures
+
+The code creates an array with the user's choices, but unfortunately I was unable to create the for loop that pulls a random character out of the array. I tried various methods for at least 3 hours, but I couldn't figure it out. 
+
+I tried my hardest, and I will resubmit if necessary.
+
+Here is the link to my deployed site: 
+https://lola-violet.github.io/password-generator/
--- script.js
+++ script.js
@@ -1,5 +1,119 @@
+// Lowercase array
+var optLowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; // 0-25 (26 total)
+console.log(optLowerCase);
+// Uppercase array
+var optUpperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]; // 0-25 (26 total)
+console.log(optUpperCase);
+// Numbers array
+var optNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]; // 0-9 (10 total)
+console.log(optNumbers);
+// Special Characters array
+var optSpecialChars = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "{", "|", "}", "~"]; // 0-26 (27 total)
+console.log(optSpecialChars);
+// Empty array
+var options = [];
+var pass = [];
+console.log(options);
+
+
+// Booleans
+var pwLower = true;
+var pwUpper = true;
+var pwNumber = true; 
+var pwSpecial = true;
+
+
+
 function generatePassword(){
     //TODO: your code goes here
+    // Ask user for length of password
+    var pwLength = prompt("How many characters do you want your password to have? (Number must be between 8-128)");
+    console.log(pwLength);
+    if (pwLength < 8 || pwLength > 128) {
+        var errorMsg = alert("The number you've chosen is outside of the parameters. Please try again.");
+        return;
+    }
+    if (pwLength >= 8 && pwLength <= 128) {
+        // Confirm inclusion of lowercase letters
+        var pwLower = confirm("Include lowercase letters?");
+        // Confirm inclusion of uppercase letters
+        var pwUpper = confirm("Include uppercase letters?");
+        // Confirm inclusion of numbers
+        var pwNumber = confirm("Include numbers?");
+        // Confirm inclusion of special characters
+        var pwSpecial = confirm("Include special characters?");
+    }
+    // If all are denied 
+    if (!pwLower && !pwUpper && !pwNumber && !pwSpecial) {
+        var errorMsg2 = alert("You must choose at lease one option. Please try again.");
+        var pwLower = confirm("Include lowercase letters?");
+        var pwUpper = confirm("Include uppercase letters?");
+        var pwNumber = confirm("Include numbers?");
+        var pwSpecial = confirm("Include special characters?");
+    }
+    // If all are confirmed
+    if (pwLower && pwUpper && pwNumber && pwSpecial) {
+        var bank = options.concat(optLowerCase, optUpperCase, optNumbers, optSpecialChars);
+    }
+    // If one is denied
+    else if (pwLower && pwUpper && pwNumber) {
+        var bank = options.concat(optLowerCase, optUpperCase, optNumbers);
+    }
+    else if (pwLower && pwUpper && pwSpecial) {
+        var bank = options.concat(optLowerCase, optUpperCase, optSpecialChars);
+    }
+    else if (pwLower && pwNumber && pwSpecial) {
+        var bank = options.concat(optLowerCase, optNumbers, optSpecialChars);
+    }
+    else if (pwUpper && pwNumber && pwSpecial) {
+        var bank = options.concat(optUpperCase, optNumbers, optSpecialChars);
+    }
+    // if two are denied
+    else if (pwLower && pwUpper) {
+        var bank = options.concat(optLowerCase, optUpperCase);
+    }
+    else if (pwLower && pwNumber) {
+        var bank = options.concat(optLowerCase, optNumbers);
+    }
+    else if (pwLower && pwSpecial) {
+        var bank = options.concat(optLowerCase, optSpecialChars);
+    }
+    else if (pwUpper && pwNumber) {
+        var bank = options.concat(optUpperCase, optNumbers);
+    }
+    else if (pwUpper && pwSpecial) {
+        var bank = options.concat(optUpperCase, optSpecialChars);
+    }
+    else if (pwNumber && pwSpecial) {
+        var bank = options.concat(optNumbers, optSpecialChars);
+    }
+    // if three are denied
+    else if (pwLower) {
+        var bank = options.concat(optLowerCase);
+    }
+    else if (pwUpper) {
+        var bank = options.concat(optUpperCase);
+    }
+    else if (pwNumber) {
+        var bank = options.concat(optNumbers);
+    }
+    else if (pwSpecial) {
+        var bank = options.concat(optSpecialChars);
+    }
+
+    console.log(bank); 
+
+    //This is where I get completely lost.
+    // I know I need to have a for loop with the user input length & use the math element that pulls a random character out of the bank array I created. I've tried several setups and I can't figure it out. 
+    
+    for (let i = 0; i < pwLength; i++) {
+    bank[Math.floor(Math.random() * pwLength)];
+    }
+        
+    
+    console.log(bank); 
+    
+
     return "password"
   }
   
@@ -18,4 +132,4 @@ function generatePassword(){
   
   // Add event listener to generate button
   generateBtn.addEventListener("click", writePassword);
-  
+
